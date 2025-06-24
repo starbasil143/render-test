ea5389064f67629dd4639aefc733648644dacfa5
starbasil <167671623+starbasil143@users.noreply.github.com>
2025-06-24 02:02:06 -0500

    make the  backend!?

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+morgan.token('body', (req, res) => req.method==="POST"?JSON.stringify(req.body):"");
+
+app.use(express.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+let data = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request,response)=>{
+  response.send('<h1>I know where you reside.</h1>');
+})
+
+app.get('/sine', (request,response)=>{
+  response.send('<h1>Cosine.</h1>');
+})
+
+app.get('/api/persons', (request,response)=>{
+  response.json(data);
+})
+
+app.post('/api/persons', (request, response)=>{
+  const personData = request.body;
+  
+  if (data.some(p=>p.name===personData.name)) {
+    response.json({ error: 'Name is already in use.' });
+    response.status(400).end();
+  } else if (!personData.name || !personData.number) {
+    response.json({ error: "Request must include fields 'name' and 'number'" });
+    response.status(400).end();
+  } else
+  {
+    const newPerson = {
+      name: request.body.name,
+      number: request.body.number,
+    }
+    
+    do {
+      newPerson.id = Math.floor(Math.random()*143143143)
+    } while (data.some(p=>p.id===newPerson.id)) 
+      
+      data = data.concat(newPerson);
+      response.json(newPerson);
+    }
+  })
+  
+  app.get('/api/persons/:id', (request,response)=>{
+    const id = request.params.id;
+    
+    const person = data.find(x=>x.id===id)
+    
+    if (person){
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  })
+  
+  app.delete('/api/persons/:id', (request, response)=>{
+    const id = request.params.id;
+    
+    data = data.filter(p=>p.id!==id);
+    response.status(204).end;
+  })
+  
+  app.get('/info', (request,response)=>{
+    response.send(`
+      <p>Phonebook has info for ${data.length} people</p>
+      <p>${Date()}</p>
+      `);
+    })
+    
+    
+    const PORT = process.env.PORT || 3001
+    app.listen(PORT, ()=>{
+      console.log(`Server running on port ${PORT}`);
+    })
